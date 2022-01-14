52eab157607a3bb575e0c94b4aeb5b9ccb9137e2
Dennet Cordelia <decordel@ge-j3.kzn.21-school.ru>
2022-01-14 22:36:28 +0300

    fix

--- push_swap/main.c
+++ push_swap/main.c
@@ -6,11 +6,90 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 18:44:18 by decordel          #+#    #+#             */
-/*   Updated: 2021/12/14 18:44:46 by decordel         ###   ########.fr       */
+/*   Updated: 2022/01/14 21:43:37 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(int argc, char const **argv)
+#include "inc/push_swap.h"
+#include <stdio.h>
+
+static void	print_content(t_stack *stack)
 {
+	if (stack)
+		ft_putnbr_fd(stack->n, 1);
+	ft_putstr_fd("\t|", 1);
+	if (stack)
+		ft_putnbr_fd(stack->index, 1);
+}
+
+void	print_stacks(t_data *data)
+{
+	t_stack	*tmp_a;
+	t_stack	*tmp_b;
+
+	tmp_a = data->a;
+	tmp_b = data->b;
+	ft_putstr_fd("\033[1;32m[-------------------------------|\n", 1);
+	ft_putstr_fd("|\t\033[1;31mA\033[1;32m\t|\t\033[1;34mB\033[1;32m\t|\n", 1);
+	ft_putstr_fd("| \033[1;31mn\t| index\033[1;32m\t", 1);
+	ft_putstr_fd("| \033[1;34mn\t| index\033[1;32m\t|\n", 1);
+	ft_putstr_fd("|\033[1;31m-------|-------\033[1;32m", 1);
+	ft_putstr_fd("|\033[1;34m-------|-------\033[1;32m|\n|\033[1;31m", 1);
+	while (tmp_a || tmp_b)
+	{
+		print_content(tmp_a);
+		ft_putstr_fd("\t\033[1;32m|\033[1;34m", 1);
+		print_content(tmp_b);
+		ft_putstr_fd("\t\033[1;32m|\n|\033[1;31m", 1);
+		if (tmp_a)
+			tmp_a = tmp_a->next;
+		if (tmp_b)
+			tmp_b = tmp_b->next;
+	}
+	ft_putstr_fd("\033[1;32m-------------------------------]\033[0m\n\n", 1);
+}
+
+void	init_a(t_data *data, char **argv)
+{
+	size_t	index;
+	size_t	i;
+	size_t	j;
+	t_stack	*tmp;
+	char	**tmp_arg;
+
+	i = 1;
+	index = 1;
+	while (argv[i])
+	{
+		tmp_arg = ft_split(argv[i++], ' ');
+		j = 0;
+		while (tmp_arg[j])
+		{
+			tmp = new_stack(ft_atoi(tmp_arg[j++]), index++);
+			ft_stackadd_back(&(data->a), tmp);
+		}
+		split_free(tmp_arg);
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	t_data	data;
+
+	if (argc == 1)
+		return (0);
+	data.a = NULL;
+	data.b = NULL;
+	data.count_oper = 0;
+	init_a(&data, argv);
+	check_num_in_args(argv);
+	if (find_dup(data.a, data.a->n))
+		ft_put_err("Error\ndup");
+	// print_stacks(&data);
+	push(&data.a, &data.b);
+	push(&data.a, &data.b);
+	// print_stacks(&data);
+	ft_stackclear(&(data.a));
+	ft_stackclear(&(data.b));
 	return (0);
 }
---
+++ push_swap/src/stack_com.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_com.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/12 18:57:30 by decordel          #+#    #+#             */
+/*   Updated: 2022/01/14 19:54:30 by decordel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+t_stack	*new_stack(int n, size_t i)
+{
+	t_stack	*new;
+
+	new = (t_stack *)malloc(sizeof (new));
+	if (!new)
+		return (NULL);
+	new->index = i;
+	new->n = n;
+	new->next = NULL;
+	return (new);
+}
+
+t_stack	*ft_stclast(t_stack *lst)
+{
+	t_stack	*tmp;
+
+	tmp = lst;
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
+void	ft_stackadd_back(t_stack **lst, t_stack *newl)
+{
+	t_stack	*head;
+
+	head = *lst;
+	if (!head)
+	{
+		*lst = newl;
+		return ;
+	}
+	head = ft_stclast(head);
+	head->next = newl;
+}
+
+void	ft_stackclear(t_stack **stack)
+{
+	t_stack	*head;
+	t_stack	*next;
+
+	head = *stack;
+	if (stack)
+	{
+		while (head)
+		{
+			next = head->next;
+			free(head);
+			head = next;
+		}
+	}
+	*stack = NULL;
+}
