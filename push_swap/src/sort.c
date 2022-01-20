664aca498854cd2a9e448a97f6fbc71ac35d02b2
RouDenr <decordel@student.21-school.ru>
2022-01-20 06:49:20 +0300

    500 : 10000

--- push_swap/src/sort.c
+++ push_swap/src/sort.c
@@ -6,75 +6,120 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/14 22:47:05 by decordel          #+#    #+#             */
-/*   Updated: 2022/01/16 22:11:37 by decordel         ###   ########.fr       */
+/*   Updated: 2022/01/20 06:44:24 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/push_swap.h"
 
-void	do_com(t_data *data, char *com)
+void	push_more_middle(t_data *data, t_stack **in, t_stack **out)
 {
-	ft_putendl_fd(com, 1);
-	data->count_oper++;
-	// print_stacks(data);
+	t_stack	*middle;
+	int		size;
+	int		tmp_size;
+
+	middle = middle_index_steck(*in);
+	size = ft_stccount(*in) / 2 + 1;
+	tmp_size = ft_stccount(*in);
+	while (tmp_size != size)
+	{
+		if ((*in)->n >= middle->n)
+		{
+			do_push(data, *out);
+			tmp_size = ft_stccount(*in);
+		}
+		else
+			do_rotate(data, *in);
+	}
 }
 
-void	sort_three(t_data *data, t_stack *stack)
+void	push_less_middle(t_data *data, t_stack **in, t_stack **out)
 {
-	t_stack	*min;
+	t_stack	*middle;
+	int		size;
+	int		tmp_size;
 
-	min = ft_stack_which_min_index(stack);
-	if (stack == min)
-		do_rotate(data, stack);
-	else if (stack->index == min->index + 1)
-		do_reverse(data, stack);
-	else if (stack->next == min)
-		do_rotate(data, stack);
-	else
-		do_swap(data, stack);
+	middle = middle_index_steck(*in);
+	size = ft_stccount(*in) / 2;
+	tmp_size = ft_stccount(*in);
+	while (tmp_size != size)
+	{
+		if ((*in)->n < middle->n)
+		{
+			do_push(data, *out);
+			tmp_size = ft_stccount(*in);
+		}
+		else
+			do_rotate(data, *in);
+	}
 }
 
-void	min_to_b(t_data *data, t_stack *stack)
+t_stack	*middle_index_steck_without_end(t_stack *stack)
 {
-	t_stack	*min;
+	int	i;
+	int	j;
 
-	min = ft_stack_which_min_index(stack);
-	if (is_first_part_stack(stack, min))
-		rotate_to_top(data, stack, min);
-	else
-		reverse_to_top(data, stack, min);
-	while (!check_sort_stack(data->a))
-		sort_a(data);
-	while (data->b)
-		do_push(data, data->a);
+	i = ft_stack_which_max_index(stack)->index;
+	j = ft_stclast(stack)->index + 1;
+	i = (i + j) / 2 + 1;
+	return (ft_stack_which_index(stack, i));
 }
 
-void	sort_first_three(t_data *data, t_stack *stack)
+void	push_less_middle_without_end(t_data *data, t_stack **in, t_stack **out)
 {
-	(void) data;
-	(void) stack;
+	t_stack	*middle;
+	t_stack	*end;
+	int		size;
+	int		tmp_size;
+
+	end = ft_stclast(*in);
+	middle = middle_index_steck_without_end(*in);
+	size = (ft_stccount(*in) - end->index) / 2 + end->index;
+	if (data->a->next->index == 1)
+	{
+		do_rotate(data, data->a);
+		return ;
+	}
+	tmp_size = ft_stccount(*in);
+	while (tmp_size != size)
+	{
+		if ((*in)->n < middle->n)
+		{
+			do_push(data, *out);
+			tmp_size = ft_stccount(*in);
+		}
+		else
+			do_rotate(data, *in);
+	}
+	while (!end_sort(*in))
+		do_reverse(data, *in);
 }
 
 void	sorting(t_data *data)
 {
-	sort_a(data);
-	while (data->b)
-		do_push(data, data->a);
-}
-
-void	sort_a(t_data *data)
-{
 	int	count;
 
-	while (!check_sort_stack(data->a))
+	while (!check_sort_data(data))
 	{
 		count = ft_stccount(data->a);
-		if (count == 2)
-			do_rotate(data, data->a);
-		else if (count == 3)
-			sort_three(data, data->a);
-		else
-			ft_push_to_top(data, data->a, ft_stack_which_min_index(data->a));
-		// print_stacks(data);
+		if (count > 5)
+		{
+			if (!end_sort(data->a))
+				push_less_middle(data, &data->a, &data->b);
+			else
+				push_less_middle_without_end(data, &data->a, &data->b);
+		}
+		count = ft_stccount(data->a);
+		if (count <= 5)
+			sort_less_five_a(data, count);
+		// while (data->b)
+		// {
+		count = ft_stccount(data->b);
+		if (count > 5)
+			push_more_middle(data, &data->b, &data->a);
+		count = ft_stccount(data->b);
+		// if (count <= 50)
+			sort_less_five_b(data, count);
+		// }
 	}
 }
