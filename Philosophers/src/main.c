68c7b30197e19810459a8a870b0217a278a4d06c
Dennet Cordelia <decordel@ge-i6.kzn.21-school.ru>
2022-02-03 01:51:51 +0300

    fix

--- Philosophers/src/main.c
+++ Philosophers/src/main.c
@@ -1,28 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_x_type.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/25 22:19:46 by decordel          #+#    #+#             */
-/*   Updated: 2021/11/01 17:04:14 by decordel         ###   ########.fr       */
+/*   Created: 2022/02/03 01:27:42 by decordel          #+#    #+#             */
+/*   Updated: 2022/02/03 01:50:57 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "../inc/philo.h"
 
-void	ft_x_type(unsigned int n, const char *base, int z, size_t *counter)
+int	main(int argc, char **argv)
 {
-	if (z && !n)
-	{
-		write(1, &base[n & 15], 1);
-		(*counter)++;
-	}
-	if (n)
-	{
-		ft_x_type(n >> 4, base, 0, counter);
-		write(1, &base[n & 15], 1);
-		(*counter)++;
-	}
+
+	return (0);
 }
---
+++ Philosophers/src/philo.c
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 16:30:08 by decordel          #+#    #+#             */
+/*   Updated: 2022/01/29 14:36:09 by decordel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+t_philo_info	*get_philo(t_data *data, t_philo *philo)
+{
+	t_philo_info	*info;
+
+	info = (t_philo_info *)malloc(sizeof (t_philo_info));
+	info->data = data;
+	info->philo = philo;
+	return (info);
+}
+
+t_philo	*new_philo(void)
+{
+	t_philo		*new;
+
+	new = (t_philo *)malloc(sizeof (t_philo));
+	if (!new)
+		return (NULL);
+	new->fork_free = 1;
+	new->time_eat = 0;
+	new->time_die = 0;
+	new->time_sleep = 0;
+	new->next = NULL;
+	return (new);
+}
+
+t_philo	*ft_lstlast(t_philo *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
+void	ft_lstadd_back(t_philo **lst, t_philo *newl)
+{
+	t_philo	*head;
+
+	head = *lst;
+	if (!head)
+	{
+		*lst = newl;
+		return ;
+	}
+	head = ft_lstlast(head);
+	head->next = newl;
+}
+
+void	ft_philoclear(t_philo **stack)
+{
+	t_philo	*head;
+	t_philo	*next;
+
+	head = *stack;
+	if (stack)
+	{
+		while (head)
+		{
+			next = head->next;
+			free(head);
+			head = next;
+		}
+	}
+	*stack = NULL;
+}
