d9d986334cb881f9ad1c4af5e18d02de85e92ce6
Dennet Cordelia <decordel@ge-i6.kzn.21-school.ru>
2022-02-03 02:19:08 +0300

    philo eat and sleep, not die

--- Philosophers/src/main.c
+++ Philosophers/src/main.c
@@ -6,14 +6,74 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 01:27:42 by decordel          #+#    #+#             */
-/*   Updated: 2022/02/03 01:50:57 by decordel         ###   ########.fr       */
+/*   Updated: 2022/02/03 02:15:25 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-int	main(int argc, char **argv)
+void	data_init(t_data *data, const char **argv)
 {
+	int	i;
 
+	pthread_mutex_init(&(data->mutex), NULL);
+	data->num_philo = ft_atoi(argv[1]);
+	data->n_time_eat = ft_atoi(argv[2]);
+	data->n_time_die= ft_atoi(argv[3]);
+	data->n_time_sleep= ft_atoi(argv[4]);
+	if (argv[5])
+		data->n_num_philo_eat = ft_atoi(argv[5]);
+	else
+		data->n_num_philo_eat = -1;
+	data->first_philo = NULL;
+	i = 0;
+	while (i++ < data->num_philo)
+		ft_lstadd_back(&(data->first_philo), new_philo());
+}
+
+int	philo_init(t_data *data)
+{
+	t_philo	*philo;
+
+	philo = data->first_philo;
+	while (philo)
+	{
+		if (pthread_create(&(philo->tid), NULL, &born_philo,
+			get_philo(data, philo)) != 0)
+			{
+				printf("Pthread create error");
+				return (0);
+			}
+		philo = philo->next;
+	}
+}
+
+int	philo_join(t_data *data)
+{
+	t_philo	*philo;
+
+	philo = data->first_philo;
+	while (philo)
+	{
+		if (pthread_join(philo->tid, NULL) != 0)
+			{
+				printf("Pthread join error");
+				return (0);
+			}
+		philo = philo->next;
+	}
+}
+
+int	main(int argc, const char **argv)
+{
+	t_data	data;
+
+	if (!valid(argc, argv))
+		return (0);
+	data_init(&data, argv);
+	philo_init(&data);
+	// print_philos(&data);
+	philo_join(&data);
+	ft_philoclear(&(data.first_philo));
 	return (0);
 }
