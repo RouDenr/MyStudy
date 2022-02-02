27277982b9d4a6cd6df5d78df3ac18902491ebb2
Dennet Cordelia <decordel@ge-i6.kzn.21-school.ru>
2022-02-03 01:51:51 +0300

    fix

---
+++ Philosophers/inc/philo.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/26 16:20:37 by decordel          #+#    #+#             */
+/*   Updated: 2022/01/29 14:35:51 by decordel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+# include <libc.h>
+# include <pthread.h>
+
+typedef struct s_philo
+{
+	pthread_t		tid;
+	int				fork_free;
+	int				time_eat;
+	int				time_die;
+	int				time_sleep;
+	struct s_philo	*next;
+
+}					t_philo;
+
+typedef struct s_data
+{
+	pthread_mutex_t	mutex;
+	int				num_philo;
+	int				n_time_eat;
+	int				n_time_die;
+	int				n_time_sleep;
+	int				n_num_philo_eat;
+	t_philo			*first_philo;
+}					t_data;
+
+typedef struct s_philo_info
+{
+	t_data	*data;
+	t_philo	*philo;
+}	t_philo_info;
+
+void			print_philos(t_data *data);
+int				valid(int c, const char**v);
+int				ft_atoi(const char *str);
+t_philo			*new_philo(void);
+void			ft_lstadd_back(t_philo **lst, t_philo *newl);
+void			ft_philoclear(t_philo **stack);
+t_philo_info	*get_philo(t_data *data, t_philo *philo);
+void			*born_philo(void *tmp);
+
+#endif
--- Philosophers/src/valid.c
+++ Philosophers/src/valid.c
@@ -1,22 +1,49 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*   valid.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/06 14:27:31 by decordel          #+#    #+#             */
-/*   Updated: 2021/12/08 00:09:03 by                  ###   ########.fr       */
+/*   Created: 2022/01/28 16:25:07 by decordel          #+#    #+#             */
+/*   Updated: 2022/01/28 16:43:40 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_isalnum(int c)
+#include "../inc/philo.h"
+
+int	ft_isdigit(int c)
 {
 	if (c >= '0' && c <= '9')
 		return (1);
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	if (c >= 'A' && c <= 'Z')
-		return (1);
 	return (0);
+}
+
+int	valid(int c, const char **v)
+{
+	int	i;
+	int	j;
+
+	if (c < 5 || c > 6)
+	{
+		printf("./philo number_of_philosophers time_to_die time_to_eat\
+ time_to_sleep [number_of_times_each_philosopher_must_eat]");
+		return (0);
+	}
+	i = 1;
+	while (i < c)
+	{
+		j = 0;
+		while (v[i][j])
+		{
+			if (!ft_isdigit(v[i][j]))
+			{
+				printf("arg %d not valid\n", i);
+				return (0);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
 }
