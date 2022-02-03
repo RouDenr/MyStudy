c9d1c3a193a367518ef4f6884e1515c475333218
Dennet Cordelia <decordel@ge-j3.kzn.21-school.ru>
2022-02-03 22:47:36 +0300

    add monitoring, philos eat norm

--- Philosophers/src/philo_activity.c
+++ Philosophers/src/philo_activity.c
@@ -6,7 +6,7 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 21:22:16 by decordel          #+#    #+#             */
-/*   Updated: 2022/02/03 02:35:01 by decordel         ###   ########.fr       */
+/*   Updated: 2022/02/03 22:44:57 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,28 @@
 
 int	forks_free(t_data *data, t_philo *philo)
 {
-	if (philo->next)
+	if (philo->next != NULL)
 		return (philo->fork_free && philo->next->fork_free);
-	else
+	else if (data->first_philo != philo)
 		return (philo->fork_free && data->first_philo->fork_free);
+	return (0);
 }
 
-int	philo_eat(t_data *data, t_philo *philo, t_philo *philo_r)
+void	philo_eat(t_data *data, t_philo *philo, t_philo *philo_r)
 {
-	printf("%d philo eat...\n", philo->n_p);
+	printf("%d %d has taken a fork\n", time_now(data), philo->n_p);
 	philo->fork_free = 0;
 	philo_r->fork_free = 0;
-	// print_philos(data);
+	printf("%d %d is eating\n", time_now(data), philo->n_p);
 	pthread_mutex_unlock(&(data->mutex));
 	usleep(data->n_time_eat * 1000);
-	printf("%d philo sleep...\n", philo->n_p);
-	usleep(data->n_time_sleep * 1000);
-	printf("%d philo think...\n", philo->n_p);
+	philo->count_eat++;
 	philo->fork_free = 1;
 	philo_r->fork_free = 1;
-	return (1);
+	printf("%d %d is sleeping\n", time_now(data), philo->n_p);
+	philo->time_die = time_now(data) + data->n_time_sleep;
+	usleep(data->n_time_sleep * 1000);
+	printf("%d %d is thinking\n", time_now(data), philo->n_p);
 }
 
 int	take_forks(t_data *data, t_philo *philo)
@@ -41,13 +43,13 @@ int	take_forks(t_data *data, t_philo *philo)
 	pthread_mutex_lock(&(data->mutex));
 	if (forks_free(data, philo))
 	{
-		printf("philo eat...\n");
 		if (philo->next)
 			philo_eat(data, philo, philo->next);
 		else
 			philo_eat(data, philo, data->first_philo);
 	}
-	pthread_mutex_unlock(&(data->mutex));
+	else
+		pthread_mutex_unlock(&(data->mutex));
 	return (0);
 }
 
@@ -59,12 +61,12 @@ void	*born_philo(void *tmp)
 	data = ((t_philo_info *)tmp)->data;
 	philo = ((t_philo_info *)tmp)->philo;
 	free(tmp);
-	printf("%d philo think...\n", philo->n_p);
-	while (1)
+	printf("%d %d is thinking\n", time_now(data), philo->n_p);
+	philo->time_die = time_now(data);
+	while (data->n_num_philo_eat)
 	{
 		if (forks_free(data, philo))
 			take_forks(data, philo);
 	}
-
 	return (tmp);
 }
---
+++ Philosophers/src/time.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/03 20:26:34 by decordel          #+#    #+#             */
+/*   Updated: 2022/02/03 20:56:43 by decordel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+int	get_time(void)
+{
+	struct timeval	time;
+
+	gettimeofday(&time, NULL);
+	return ((time.tv_sec * 1000000 + time.tv_usec) / 1000);
+}
+
+int	time_now(t_data *data)
+{
+	return (get_time() - data->time);
+}
