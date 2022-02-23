80e1a2f9f360783d52b38caea0fa57fb59104de5
RouDenr <decordel@student.21-school.ru>
2022-02-24 00:26:11 +0300

    finish man part

--- Philosophers/philo/src/time.c
+++ Philosophers/philo/src/time.c
@@ -6,19 +6,18 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 20:26:34 by decordel          #+#    #+#             */
-/*   Updated: 2022/02/20 20:44:05 by decordel         ###   ########.fr       */
+/*   Updated: 2022/02/24 00:08:12 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo.h"
 
-int	get_time(void)
+unsigned long long	get_time(void)
 {
 	struct timeval	time;
 
 	gettimeofday(&time, NULL);
-	return ((time.tv_sec * 1000000 + time.tv_usec) / 1000);
-	// return (time.tv_usec / 1000);
+	return ((time.tv_sec * (unsigned long long) 1000) + (time.tv_usec / 1000));
 }
 
 int	time_now(t_data *data)
