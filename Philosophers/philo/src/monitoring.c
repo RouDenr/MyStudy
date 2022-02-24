1db2ba7812b7fed1f482a5d3b8180a2ad4ca0ec2
Dennet Cordelia <decordel@va-b1.kzn.21-school.ru>
2022-02-24 17:17:00 +0300

    fix usleep

--- Philosophers/philo/src/monitoring.c
+++ Philosophers/philo/src/monitoring.c
@@ -6,7 +6,7 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 21:08:04 by decordel          #+#    #+#             */
-/*   Updated: 2022/02/24 00:22:36 by decordel         ###   ########.fr       */
+/*   Updated: 2022/02/24 17:13:22 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	all_philo_ate(t_data *data)
 		philo = philo->next;
 	}
 	data->n_num_philo_eat = 0;
-	philo_detach(data);
+	// philo_detach(data);
 	printf("\033[38;05;46m%d ALL ATE\033[0m\n", time_now(data));
 	return (1);
 }
@@ -40,7 +40,7 @@ int	all_philo_alive(t_data *data)
 		if (time_now(data) - philo->last_eat > data->n_time_die)
 		{
 			data->n_num_philo_eat = 0;
-			philo_detach(data);
+			// philo_detach(data);
 			printf("\033[38;05;196m%d %d IS DEAD\033[0m\n",
 				time_now(data), philo->n_p);
 			return (0);
@@ -72,7 +72,7 @@ void	*monitoring(void *tmp)
 	t_data	*data;
 
 	data = (t_data *)tmp;
-	data->n_time_sleep--;
+	// data->n_time_sleep--;
 	while (!all_philo_ate(data) && all_philo_alive(data))
 		;
 	return (data);
--- Philosophers/philo/src/time.c
+++ Philosophers/philo/src/time.c
@@ -6,7 +6,7 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 20:26:34 by decordel          #+#    #+#             */
-/*   Updated: 2022/02/24 00:08:12 by decordel         ###   ########.fr       */
+/*   Updated: 2022/02/24 17:05:45 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,3 +24,18 @@ int	time_now(t_data *data)
 {
 	return (get_time() - data->time);
 }
+
+void	ft_usleep(unsigned long long time)
+{
+	unsigned long long start;
+
+	start = get_time();
+	while (time > get_time() - start)
+		usleep(100);
+}
+
+// int main(int argc, char const *argv[])
+// {
+// 	ft_usleep(10000);
+// 	return 0;
+// }
