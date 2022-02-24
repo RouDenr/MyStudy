f5bda4dd10fdf50fc743f6eaf4a8f350374e4007
Dennet Cordelia <decordel@va-b1.kzn.21-school.ru>
2022-02-24 17:17:00 +0300

    fix usleep

--- Philosophers/philo/src/ft_atoi.c
+++ Philosophers/philo/src/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 11:44:33 by decordel          #+#    #+#             */
-/*   Updated: 2021/12/08 00:09:03 by                  ###   ########.fr       */
+/*   Updated: 2022/02/24 16:50:58 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,7 @@ int	ft_atoi(const char *str)
 		if (n * sign > 2147483647)
 			return (-1);
 		if (n * sign < -2147483648)
-			return (0);
+			return (-1);
 	}
 	return (n * sign);
 }
