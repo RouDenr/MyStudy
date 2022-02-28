8ad9004b46ba3304f89f48e9fed2b90e0761524b
Dennet Cordelia <decordel@et-b5.kzn.21-school.ru>
2022-03-01 02:35:05 +0300

    fix

--- Philosophers/philo_bonus/src/monitoring.c
+++ Philosophers/philo_bonus/src/monitoring.c
@@ -6,28 +6,11 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 21:08:04 by decordel          #+#    #+#             */
-/*   Updated: 2022/02/24 01:24:43 by decordel         ###   ########.fr       */
+/*   Updated: 2022/03/01 02:31:49 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/philo_bonus.h"
-
-int	all_philo_ate(t_data *data)
-{
-	t_philo	*philo;
-
-	philo = data->first_philo;
-	while (philo)
-	{
-		if (philo->count_eat < data->n_num_philo_eat
-			|| data->n_num_philo_eat < 0)
-			return (0);
-		philo = philo->next;
-	}
-	data->n_num_philo_eat = 0;
-	printf("\033[38;05;46m%d ALL ATE\033[0m\n", time_now(data));
-	return (1);
-}
 
 void	kill_philos(t_data *data)
 {
@@ -42,34 +25,36 @@ void	kill_philos(t_data *data)
 
 }
 
-int	all_philo_alive(t_data *data)
-{
-	t_philo	*philo;
-
-	philo = data->first_philo;
-	while (philo)
-	{
-		if (time_now(data) - philo->last_eat > data->n_time_die)
-		{
-			data->n_num_philo_eat = 0;
-			printf("\033[38;05;196m%d %d IS DEAD\033[0m\n",
-				time_now(data), philo->n_p);
-			return (0);
-		}
-		philo = philo->next;
-	}
-	return (1);
-}
-
-void	*monitoring(void *tmp)
+void	*die_monitoring(void *tmp)
 {
 	t_data	*data;
 
 	data = (t_data *)tmp;
-	data->n_time_sleep--;
-	while (!all_philo_ate(data) && all_philo_alive(data))
-		;
+	sem_wait(data->sems->all_alive);
+	pthread_detach(data->tid_ate);
 	kill_philos(data);
+	forks_clean(data->sems);
+	ft_philoclear(&(data->first_philo));
 	exit(0);
-	// return (data);
+}
+
+void	*ate_monitoring(void *tmp)
+{
+	t_data	*data;
+	t_philo	*philo;
+
+	data = (t_data *)tmp;
+	philo = data->first_philo;
+	while (philo)
+	{
+		sem_wait(data->sems->all_ate);
+		philo = philo->next;
+	}
+	printf("\033[38;05;46m%llu ALL ATE\033[0m\n",
+		time_now(data));
+	pthread_detach(data->tid_die);
+	kill_philos(data);
+	forks_clean(data->sems);
+	ft_philoclear(&(data->first_philo));
+	exit(0);
 }
