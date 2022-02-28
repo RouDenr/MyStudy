351ecabbc0880fde57e7bd89c4c29044cfd4883b
RouDenr <decordel@student.21-school.ru>
2022-03-01 02:35:05 +0300

    fix

--- Philosophers/philo_bonus/src/main.c
+++ Philosophers/philo_bonus/src/main.c
@@ -6,7 +6,7 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 01:27:42 by decordel          #+#    #+#             */
-/*   Updated: 2022/02/24 18:30:47 by decordel         ###   ########.fr       */
+/*   Updated: 2022/03/01 02:32:23 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,12 @@ void	data_init(t_data *data, const char **argv)
 	if (argv[5])
 		data->n_num_philo_eat = ft_atoi(argv[5]);
 	else
-		data->n_num_philo_eat = -1;
+		data->n_num_philo_eat = -5;
 	if (!data_valid(data))
 		ft_put_err("Not valid args");
 	data->time = get_time();
 	data->first_philo = NULL;
+	data->sems = new_fork(data->num_philo, data->n_time_eat);
 	i = 0;
 	while (i++ < data->num_philo)
 		ft_lstadd_back(&(data->first_philo), new_philo());
@@ -41,24 +42,33 @@ int	philo_init(t_data *data)
 	while (philo)
 	{
 		philo->pid = fork();
-		if (philo->pid)
+		if (!philo->pid)
 			born_philo(data, philo);
 		else if (philo->pid < 0)
 			ft_put_err("Fork err");
 		philo = philo->next;
 	}
-	if (pthread_create(&(data->tid), NULL, &monitoring,
-			data) != 0)
-	{
-		printf("Pthread create error");
-		return (0);
-	}
 	return (1);
+}
+
+void	monitoring_init(t_data *data)
+{
+	if (pthread_create(&(data->tid_ate), NULL, &ate_monitoring,
+			data) != 0)
+		printf("Pthread create error");
+	if (pthread_create(&(data->tid_die), NULL, &die_monitoring,
+			data) != 0)
+		printf("Pthread create error");
 }
 
 int	philo_join(t_data *data)
 {
-	if (pthread_join(data->tid, NULL) != 0)
+	if (pthread_join(data->tid_ate, NULL) != 0)
+	{
+		printf("Pthread join error");
+		return (0);
+	}
+	if (pthread_join(data->tid_die, NULL) != 0)
 	{
 		printf("Pthread join error");
 		return (0);
@@ -75,8 +85,8 @@ int	main(int argc, const char **argv)
 	data_init(&data, argv);
 	if (!philo_init(&data))
 		return (2);
+	monitoring_init(&data);
 	if (!philo_join(&data))
 		return (3);
-	ft_philoclear(&(data.first_philo));
 	return (0);
 }
--- Philosophers/philo_bonus/src/philo_activity.c
+++ Philosophers/philo_bonus/src/philo_activity.c
@@ -6,7 +6,7 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 21:22:16 by decordel          #+#    #+#             */
-/*   Updated: 2022/02/24 01:11:41 by decordel         ###   ########.fr       */
+/*   Updated: 2022/03/01 02:04:42 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,47 @@
 void	put_mess(int n_philo, char *mess, t_data *data)
 {
 	if (data->n_num_philo_eat)
-		printf("%d %d%s", time_now(data), n_philo, mess);
+		printf("");
+		printf("%llu %d%s", time_now(data), n_philo, mess);
+		(void) n_philo;
+		(void) mess;
+}
+
+void	*die_this_monitoring(void *tmp)
+{
+	t_data	*data;
+	t_philo	*philo;
+
+	data = ((t_philo_info *)tmp)->data;
+	philo = ((t_philo_info *)tmp)->philo;
+	while (time_now(data) - philo->last_eat
+		<= (unsigned long long) data->n_time_die)
+		;
+	printf("\033[38;05;196m%llu %d IS DEAD\033[0m\n",
+		time_now(data), philo->n_p);
+	sem_post(data->sems->all_alive);
+	exit(0);
 }
 
 void	philo_eat(t_data *data, t_philo *philo)
 {
 	put_mess(philo->n_p, " is eating\n", data);
 	philo->last_eat = time_now(data);
-	usleep(data->n_time_eat * 1000);
-	philo->count_eat++;
+	ft_usleep(data->n_time_eat);
+	sem_post(data->sems->forks);
+	sem_post(data->sems->forks);
+	if (++(philo->count_eat) == data->n_num_philo_eat)
+		sem_post(data->sems->all_ate);
 	put_mess(philo->n_p, " is sleeping\n", data);
-	usleep(data->n_time_sleep * 1000);
+	ft_usleep(data->n_time_sleep);
 	put_mess(philo->n_p, " is thinking\n", data);
 }
 
 int	take_forks(t_data *data, t_philo *philo)
 {
+	sem_wait(data->sems->forks);
 	put_mess(philo->n_p, " has taken a fork\n", data);
+	sem_wait(data->sems->forks);
 	put_mess(philo->n_p, " has taken a fork\n", data);
 	philo_eat(data, philo);
 	return (0);
@@ -41,7 +65,13 @@ void	born_philo(t_data *data, t_philo *philo)
 {
 	put_mess(philo->n_p, " is thinking\n", data);
 	philo->last_eat = time_now(data);
-	while (data->n_num_philo_eat)
+	if (pthread_create(&(philo->tid), NULL, &die_this_monitoring,
+			get_philo(data, philo)) != 0)
+	{
+		printf("Pthread create error");
+		exit(1);
+	}
+	while (1)
 		take_forks(data, philo);
 	exit(0);
 }
