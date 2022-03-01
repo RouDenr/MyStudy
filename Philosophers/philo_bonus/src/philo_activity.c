0a8907ef5f0b8528ef37514bbb74c58cc0bdf15e
Dennet Cordelia <decordel@et-k6.kzn.21-school.ru>
2022-03-01 21:55:10 +0300

    philo fin

--- Philosophers/philo_bonus/src/philo_activity.c
+++ Philosophers/philo_bonus/src/philo_activity.c
@@ -6,7 +6,7 @@
 /*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 21:22:16 by decordel          #+#    #+#             */
-/*   Updated: 2022/03/01 02:04:42 by decordel         ###   ########.fr       */
+/*   Updated: 2022/03/01 21:51:24 by decordel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	put_mess(int n_philo, char *mess, t_data *data)
 {
 	if (data->n_num_philo_eat)
 		printf("");
-		printf("%llu %d%s", time_now(data), n_philo, mess);
+		printf("%llu %d %s", time_now(data), n_philo, mess);
 		(void) n_philo;
 		(void) mess;
 }
@@ -30,7 +30,7 @@ void	*die_this_monitoring(void *tmp)
 	philo = ((t_philo_info *)tmp)->philo;
 	while (time_now(data) - philo->last_eat
 		<= (unsigned long long) data->n_time_die)
-		;
+		ft_usleep(10);
 	printf("\033[38;05;196m%llu %d IS DEAD\033[0m\n",
 		time_now(data), philo->n_p);
 	sem_post(data->sems->all_alive);
@@ -39,31 +39,31 @@ void	*die_this_monitoring(void *tmp)
 
 void	philo_eat(t_data *data, t_philo *philo)
 {
-	put_mess(philo->n_p, " is eating\n", data);
+	put_mess(philo->n_p, "is eating\n", data);
 	philo->last_eat = time_now(data);
 	ft_usleep(data->n_time_eat);
 	sem_post(data->sems->forks);
 	sem_post(data->sems->forks);
 	if (++(philo->count_eat) == data->n_num_philo_eat)
 		sem_post(data->sems->all_ate);
-	put_mess(philo->n_p, " is sleeping\n", data);
+	put_mess(philo->n_p, "is sleeping\n", data);
 	ft_usleep(data->n_time_sleep);
-	put_mess(philo->n_p, " is thinking\n", data);
+	put_mess(philo->n_p, "is thinking\n", data);
 }
 
 int	take_forks(t_data *data, t_philo *philo)
 {
 	sem_wait(data->sems->forks);
-	put_mess(philo->n_p, " has taken a fork\n", data);
+	put_mess(philo->n_p, "has taken a fork\n", data);
 	sem_wait(data->sems->forks);
-	put_mess(philo->n_p, " has taken a fork\n", data);
+	put_mess(philo->n_p, "has taken a fork\n", data);
 	philo_eat(data, philo);
 	return (0);
 }
 
 void	born_philo(t_data *data, t_philo *philo)
 {
-	put_mess(philo->n_p, " is thinking\n", data);
+	put_mess(philo->n_p, "is thinking\n", data);
 	philo->last_eat = time_now(data);
 	if (pthread_create(&(philo->tid), NULL, &die_this_monitoring,
 			get_philo(data, philo)) != 0)
