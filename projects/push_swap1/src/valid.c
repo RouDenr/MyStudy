156b254d038a32fedb7c9913b7eb537e8cf0d476
Dennet Cordelia <decordel@si-l4.kzn.21-school.ru>
2022-03-28 18:07:39 +0300

    as

---
+++ projects/push_swap1/src/valid.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   valid.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/14 19:54:19 by decordel          #+#    #+#             */
+/*   Updated: 2022/01/25 13:51:59 by decordel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/push_swap.h"
+
+void	check_num_in_args(char **argv)
+{
+	argv++;
+	while (*argv)
+	{
+		while (**argv)
+		{
+			if ((!ft_isdigit(**argv) && **argv != ' ' && **argv != '-')
+				|| !ft_strncmp(*argv, "- ", 2) || !ft_strncmp(*argv, "-", 2))
+				ft_put_err("Error");
+			(*argv)++;
+		}
+		argv++;
+	}
+}
+
+void	swap_index(t_stack *a, t_stack *b)
+{
+	int	tmp;
+
+	tmp = a->index;
+	a->index = b->index;
+	b->index = tmp;
+}
+
+void	sort_index(t_stack *stack)
+{
+	t_stack	*i;
+	t_stack	*j;
+	int		index;
+
+	i = stack;
+	index = 1;
+	while (i && i->next)
+	{
+		j = stack;
+		while (j)
+		{
+			if (j->index > index && i->n > j->n)
+			{
+				swap_index(i, j);
+				i = j;
+			}
+			j = j->next;
+		}
+		i = ft_stack_index(stack, ++index);
+	}
+}
+
+int	find_dup(t_stack *stack)
+{
+	t_stack	*i;
+
+	while (stack)
+	{
+		i = stack->next;
+		while (i)
+		{
+			if (i->n == stack->n)
+				return (1);
+			i = i->next;
+		}
+		stack = stack->next;
+	}
+	return (0);
+}
