b73e59264ac06b73d32ff8abfde21652e379749d
Dennet Cordelia <decordel@si-l4.kzn.21-school.ru>
2022-03-28 18:07:39 +0300

    as

---
+++ projects/push_swap1/libft/ft_atoi.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 11:44:33 by decordel          #+#    #+#             */
+/*   Updated: 2022/01/24 21:31:13 by decordel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static	int	ft_isspace(char s)
+{
+	if (s == ' ' || s == '\t' || s == '\n')
+		return (1);
+	if (s == '\v' || s == '\f' || s == '\r')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	long	n;
+	int		sign;
+
+	i = 0;
+	n = 0;
+	sign = 1;
+	while (ft_isspace(str[i]))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		n = (n * 10) + (str[i++] - '0');
+		if (n * sign > 2147483647)
+			ft_put_err("Error");
+		if (n * sign < -2147483648)
+			ft_put_err("Error");
+	}
+	return (n * sign);
+}
---
+++ projects/push_swap1/libft/ft_calloc.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: decordel <decordel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 13:45:11 by decordel          #+#    #+#             */
+/*   Updated: 2021/12/08 00:09:03 by                  ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+static void	ft_bzero(void *s, size_t n)
+{
+	char	*c;
+
+	c = s;
+	while (n-- > 0)
+	{
+		*c = '\0';
+		c++;
+	}
+}
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char		*buf;
+
+	buf = NULL;
+	if (count)
+	{
+		buf = malloc(count * size);
+		if (buf == NULL)
+			return (NULL);
+		ft_bzero(buf, count * size);
+	}
+	else
+	{
+		buf = malloc(size);
+		if (buf == NULL)
+			return (NULL);
+		ft_bzero(buf, size);
+	}
+	return (buf);
+}
